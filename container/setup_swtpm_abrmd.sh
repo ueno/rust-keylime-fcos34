0a66b57f8c5769f8b885d44fd55db7e1b78764b0
Daiki Ueno <dueno@redhat.com>
2021-10-03 15:34:31 +0200

    container/setup_swtpm_abrmd.sh: New setup script using tpm2-abrmd

---
+++ container/setup_swtpm_abrmd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pkill swtpm
+
+if test -n "$DBUS_SESSION_BUS_PID"; then
+    kill "$DBUS_SESSION_BUS_PID"
+fi
+
+eval `dbus-launch --sh-syntax --exit-with-session`
+export XDG_CONFIG_HOME=$HOME/.config
+/usr/share/swtpm/swtpm-create-user-config-files --root
+mkdir -p ${XDG_CONFIG_HOME}/mytpm1
+swtpm_setup --tpm2 --tpmstate $XDG_CONFIG_HOME/mytpm1 --createek --decryption --create-ek-cert --create-platform-cert --lock-nvram --overwrite --display
+swtpm socket --tpm2 --tpmstate dir=$XDG_CONFIG_HOME/mytpm1 --flags startup-clear --ctrl type=tcp,port=2322 --server type=tcp,port=2321 --daemon
+tpm2-abrmd --logger=stdout --tcti=swtpm: --session --allow-root --flush-all &
+export TPM2TOOLS_TCTI="tabrmd:bus_type=session"
+export TCTI=$TPM2TOOLS_TCTI
